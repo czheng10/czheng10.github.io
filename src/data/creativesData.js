563bf8861f023abac4acbc68dd263f47659679b1
Cindy Zheng <czheng03@mit.edu>
2026-09-13 01:36:26 +0000

    Reorder Creatives pieces: move the scalloped watercolor plate earlier
    Swaps the display order of the ring dish and the blue/green scalloped
    plate so the plate leads.

--- src/data/creativesData.js
+++ src/data/creativesData.js
@@ -16,10 +16,10 @@ import piece8 from '../assets/creatives/piece-8.jpg'
 const PLACEHOLDER_BACK = 'More about this piece is coming soon.'
 
 export const creativesData = [
-  { id: 1, image: piece1, width: 1400, height: 1050, alt: 'Handmade ceramic piece by Cindy Zheng', title: 'Piece 1', back: PLACEHOLDER_BACK },
+  { id: 4, image: piece4, width: 1050, height: 1400, alt: 'Handmade ceramic piece by Cindy Zheng', title: 'Piece 4', back: PLACEHOLDER_BACK },
   { id: 2, image: piece2, width: 1400, height: 1050, alt: 'Handmade ceramic piece by Cindy Zheng', title: 'Piece 2', back: PLACEHOLDER_BACK },
   { id: 3, image: piece3, width: 1050, height: 1400, alt: 'Handmade ceramic piece by Cindy Zheng', title: 'Piece 3', back: PLACEHOLDER_BACK },
-  { id: 4, image: piece4, width: 1050, height: 1400, alt: 'Handmade ceramic piece by Cindy Zheng', title: 'Piece 4', back: PLACEHOLDER_BACK },
+  { id: 1, image: piece1, width: 1400, height: 1050, alt: 'Handmade ceramic piece by Cindy Zheng', title: 'Piece 1', back: PLACEHOLDER_BACK },
   { id: 5, image: piece5, width: 1050, height: 1400, alt: 'Handmade ceramic piece by Cindy Zheng', title: 'Piece 5', back: PLACEHOLDER_BACK },
   { id: 6, image: piece6, width: 1050, height: 1400, alt: 'Handmade ceramic piece by Cindy Zheng', title: 'Piece 6', back: PLACEHOLDER_BACK },
   { id: 7, image: piece7, width: 1050, height: 1400, alt: 'Handmade ceramic piece by Cindy Zheng', title: 'Piece 7', back: PLACEHOLDER_BACK },
